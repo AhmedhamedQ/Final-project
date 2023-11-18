dba181b6579e373c9a0886243199a3dd127c5ea3
Ahmed hamed <121791737+AhmedhamedQ@users.noreply.github.com>
2023-11-18 23:45:20 +0200

    Update model_deployment.py

--- model_deployment.py
+++ model_deployment.py
@@ -13,67 +13,91 @@ with open("background.css") as f:
 model = joblib.load('Model.pkl')
 # import columns name
 columns = joblib.load('columns.pkl')
-# import encoding map 
-encoder = joblib.load('Encoder_map.pkl')
 
 
-def prediction(job,marital,education,housing, loan,contact,day_of_week,duration,campaign,pdays,emp,age_catigories):
+# define function for age 
+def classify_age(age):
+    if age < 18:
+        return "Teenagers"
+    elif age < 30:
+        return "Youth"
+    elif age < 45:
+        return "Young Adults"
+    elif age < 65:
+        return "Adults"
+    else:
+        return "Elderly"
+
+# define function for month 
+def classify_season(month):
+    if month in ['dec', 'jan', 'feb']:
+        return "Winter"
+    elif month in ['mar', 'apr', 'may']:
+        return "Spring"
+    elif month in ['jun', 'jul','aug']:
+        return "Summer"
+    elif month in ['sep','oct', 'nov']:
+        return "Autumn"
+    else:
+        return "Invalid month"
+
+
+def prediction(job, marital, education, housing, loan, contact,day_of_week,duration,campaign,pdays,previous,poutcom,emp,price,conf, euribor3m,employed,month, age):
     df = pd.DataFrame(columns = columns)
-    df.at[0,'job'] = job
-    df.at[0,'marital'] = marital
-    df.at[0,'education'] = education
-    df.at[0,'housing'] = housing
-    df.at[0,'loan'] = loan
-    df.at[0,'contact'] = contact
-    df.at[0,'day_of_week'] = day_of_week
-    df.at[0,'duration'] = duration 
-    df.at[0,'campaign'] = campaign
+    df.at[ 0 , 'age_catigories' ] = classify_age(age)
+    df.at[0, 'duration'] = duration
+    df.at[0 , 'campaign'] = campaign
     df.at[0,'pdays'] = pdays
+    df.at[0,'previous'] = previous
+    df.at[0,'job'] = job
+    df.at[0,'loan'] = loan
+    df.at[0,'housing'] = housing
+    df.at[0,'season'] = classify_season(month)
+    df.at[0,'contact'] = contact
+    df.at[0,'euribor3m'] = euribor3m
+    df.at[0,'poutcom'] = poutcom
+    df.at[0,'nr.employed'] = employed
     df.at[0,'emp.var.rate'] = emp
-    df.at[0,'age_catigories'] = age_catigories
-    for key in ['job','marital','education','housing','loan','contact','day_of_week','age_catigories']:
-        df[key] = df[key].map(encoder[key])
+    df.at[0,'cons.price.idx'] = price
+    df.at[0,'marital'] = marital
+    df.at[0,'day_of_week'] = day_of_week
+    df.at[0,'education'] = education
+    df.at[0,'cons.conf.idx'] = conf
     
     res = model.predict(df)
     return res[0]
-    
+
 
 st.title("Pridect if the client will subscribe a term deposit")
-col1 , col2 , col3 = st.columns([1,0.25 ,1])
+col1 , col2 , col3 = st.columns([1,0.25 ,2])
 with col1:
-    job = st.selectbox("job type" ,encoder['job'].keys() )
-with col3 :
-    marital = st.selectbox("marital status" ,encoder['marital'].keys() )
-col4 , col5 , col6 = st.columns([1,0.25,1])
-with col4 :
-    education = st.selectbox("education degree" ,encoder['education'].keys() )
-with col6 :
-    housing = st.selectbox("housing loan" , ['yes' , 'no'])
-col7 , col8 , col9 = st.columns([1,0.25 , 1])
-with col7 :
-    loan = st.selectbox("loan" ,['no', 'yes'] )
-with col9 :
-    contact = st.selectbox('type of contact',encoder['contact'].keys())
-col10 , col11 , col12 = st.columns([1,0.25,1])
-with col10 :
-    day_of_week = st.selectbox('select day of call' , encoder['day_of_week'].keys())
-with col12 :
-    st.subheader(' if age < 18 select Teenagers / age < 30: select Youth / age < 45 select Young Adultsel / age < 65 select Adults / age > 65 select Elderly')
-    age_catigories = st.selectbox('select age catigory', encoder['age_catigories'].keys()) 
-col13 , col14 , col15 = st.columns([1,0.25,1])
-with col13 :
-    campaign = st.slider("number of call during campign" ,min_value=1,max_value=45,step = 1 , value = 1 )
-with col15 :
+    age = st.slider('age' , min_value=12 , max_value=100 , step=1 , value=16 )
+    duration =  st.slider("duration of call" ,min_value=0,max_value=90,step = 1 , value = 1 )
+    campaign = st.slider("number of call during campign" ,min_value=1,max_value=60,step = 1 , value = 1 )
     emp = st.slider("employment variation rate" ,min_value= -4.0,max_value=2.0,step = 0.1 , value =-1.1 )
-col16 , col17 , col18 = st.columns([1,0.25,1])
-with col16 :
-    duration =  st.slider("duration of call" ,min_value=0,max_value=70,step = 1 , value = 1 )
-with col18 :
-    pdays = st.selectbox("number of days from the last call, " ,[0,1,2])
-    st.subheader('NOTE : 0 means not contacted before, 1 means contacted one day ago, and 2 means contacted two or more days ago.')
+    pdays = st.slider('Number of days since the last contact' , min_value=0 , max_value=30 ,step=1 ,value=0)
+    previous = st.slider('Number of previous contacts', min_value=0 , step = 1 , max_value=10 , value = 1)
+    euribor3m = st.slider('Monthly Triple Interest Rate', min_value=0.0 , max_value=10.0 , step = 0.1)
+    conf = st.slider('confidence interval' , min_value = -60.0 , step= 0.1 , max_value=-20.0 , value=-30.0)
+    price = st.slider('rate of change in commodity prices' , min_value = 90.0 , step= 0.1 , max_value=100.0 , value=1.0)
+    employed = st.slider('number of employee' , min_value=4900 , step=1,max_value=5250)
+
+with col3 :
+    job = st.selectbox('Type of job' , ['housemaid', 'services', 'admin.', 'blue-collar', 'technician',
+       'retired', 'management', 'unemployed', 'self-employed', 'other',
+       'entrepreneur', 'student'])
+    marital = st.selectbox('Marital status' , ['married', 'single', 'divorced', 'unknown'])
+    education= st.selectbox('Education degree',['basic.4y', 'high.school', 'basic.6y', 'basic.9y',
+       'professional.course', 'other', 'university.degree', 'illiterate'])
+    housing= st.selectbox('housing loan' , ['no', 'yes', 'unknown'])
+    loan = st.selectbox('Personal loan',['no', 'yes', 'unknown']) 
+    contact = st.selectbox('type contact' , ['telephone', 'cellular'])
+    day_of_week = st.selectbox('day' , ['mon', 'tue', 'wed', 'thu', 'fri'])
+    poutcom = st.selectbox('result of the previous campaign' , ['nonexistent', 'failure', 'success'])
+    month = st.selectbox('Month', ['dec', 'jan', 'feb','mar', 'apr', 'may','jun', 'jul','aug','sep','oct', 'nov'])
+    
 
 if st.button("Predict"):
-    result = prediction(job,marital,education,housing, loan,contact,day_of_week,duration,campaign,pdays,emp,age_catigories)
-    res_list = ["no" , "yes"]
-    st.text(res_list[result])
-    
+    result = prediction(job, marital, education, housing, loan, contact,day_of_week, duration, campaign,pdays,previous,poutcom,emp,price,conf, euribor3m,employed,month,age)
+    res_list = ["No,he will not subscribe","yes,he will subscribe"]
+    st.write(res_list[result])
